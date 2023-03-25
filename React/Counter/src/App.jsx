fdbf14e3c83a595e15d73e725fcc9cc54ba48d36
Tathagata Mishra <tathagatamishra007@gmail.com>
2023-03-25 23:52:20 +0530

    counter

---
+++ React/Counter/src/App.jsx
@@ -0,0 +1,31 @@
+
+
+
+
+function App() {
+
+  for(let i=0; i<50; i++) {
+    
+    console.log(i);
+
+    return (
+      <div align='center'>
+        
+        <h1>C O U N T E R</h1>
+        
+        <br /><br />
+        
+        <h1>{i}</h1>
+  
+        <br /><br />
+        
+        <button>click</button>
+        
+      </div>
+    )
+  }
+
+  
+}
+
+export default App
