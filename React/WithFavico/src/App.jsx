0cdd7627d1a8233707d8c64523e9b4ee22d00d75
tathagatamishra <tathsgatamishra007@gmail.com>
2023-11-04 12:58:01 +0530

    favicon test

---
+++ React/WithFavico/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </div>
+      <h1>With Favicon.ico</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
