b160b22b64f3532f800b8b673de13b7c8baec704
tathagatamishra <tathsgatamishra007@gmail.com>
2024-04-10 02:17:27 +0530

    hyh

--- React/ClickSound/src/App.jsx
+++ React/ClickSound/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from "react";
-import useSound from "use-sound";
-import audio from "./x.wav";
-import "./App.css";
+import { useState } from 'react'
+import useSound from 'use-sound';
+import buttonHoverSound from './x.wav';
+import './App.css'
 
 function App() {
-  // const [play] = useSound(audio);
-
-  let playAudio = () => {
-    new Audio(audio).play();
-  };
+  const soundUrl = buttonHoverSound;
+  const [play] = useSound(soundUrl);
 
   return (
     <div>
-      <button
-        onClick={playAudio}
-        style={{
-          padding: "10px 20px",
-          fontSize: "16px",
-          background: "#007BFF",
-          border: "none",
-          borderRadius: "5px",
-          color: "#fff",
-          fontWeight: "bold",
-          cursor: "pointer",
-          outline: "none",
-        }}
-      >
-        Hover over me!
-      </button>
+         <button
+      onClick={play}
+      style={{
+        padding: '10px 20px',
+        fontSize: '16px',
+        background: '#007BFF',
+        border: 'none',
+        borderRadius: '5px',
+        color: '#fff',
+        fontWeight: 'bold',
+        cursor: 'pointer',
+        outline: 'none',
+      }}
+    >
+      Hover over me!
+    </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
